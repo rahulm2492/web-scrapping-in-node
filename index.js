77c8c302153b26f9cb3f426f00993382f7da4146
Sumit Jangir <sumj@saxobank.com>
2019-09-13 16:14:55 +0530

    code check in

---
+++ index.js
@@ -0,0 +1,13 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const 
+
+
+const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
+
+axios(url)
+      .then(response => {
+        const html = response.data;
+        console.log(html);
+      })
+      .catch(console.error);
