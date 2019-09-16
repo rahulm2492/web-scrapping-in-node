fb5892362637768a3a9236f4b9272df56b5eea84
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-09-16 08:29:12 +0530

    index js changes

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const axios = require('axios');
+const axios = require("axios");
 const fs = require("fs");
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
-const url = 'https://www.reddit.com/r/news/';
+const url = "https://www.reddit.com/r/news/";
 
 //https://pusher.com/tutorials/web-scraper-node
 
 puppeteer
-    .launch()
-    .then(browser => browser.newPage())
-    .then(page => {
-        return page.goto(url).then(function() {
-            return page.content();
-        });
-    })
-    .then(html => {
-        const $ = cheerio.load(html);
-        const newsHeadlines = [];
-        $('a[href*="/r/news/comments"] > h2').each(function() {
-            newsHeadlines.push({
-                title: $(this).text(),
-            });
-        });
+  .launch()
+  .then(browser => browser.newPage())
+  .then(async page => {
+    await page.goto(url);
+    return page.content();
+  })
+  .then(html => {
+    const $ = cheerio.load(html);
+    // const newsHeadlines = [];
+    // $('a[href*="/r/news/comments"] > h2').each(function() {
+    //   newsHeadlines.push({
+    //     title: $(this).text()
+    //   });
+    // });
 
-        console.log(newsHeadlines);
-    })
-    .catch(console.error);
+    // console.log(newsHeadlines);
+
+    fs.writeFile("reddit.text", $.text(), err => {
+      console.log(err);
+    });
+  });
